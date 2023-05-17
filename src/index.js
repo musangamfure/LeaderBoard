b93bd5fb9ab5e6edc71e9d6aabedb06593279c67
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-05-17 11:16:16 +0100

    Update CSS and JS files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
 import './index.css';
+import {
+  createGameScores,
+  getGameScores,
+  saveGameScore,
+} from '../modules/learderboard.js';
 
-const submit = document.querySelector('.submit');
-const nameInput = document.getElementById('name');
-const scoreInput = document.getElementById('score');
-const scoresList = document.getElementById('scores');
+const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-// create a new score item for the list
-const createScores = (name, score) => {
-  const li = document.createElement('li');
-  li.textContent = `${name}: ${score}`;
-  return li;
+let gameId;
+const createGame = async () => {
+  const storedGameId = localStorage.getItem('gameId');
+  if (storedGameId) {
+    gameId = storedGameId;
+  } else {
+    const response = await fetch(`${apiUrl}games/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'My Game' }),
+    });
+    const data = await response.json();
+    gameId = data.result;
+    localStorage.setItem('gameId', gameId);
+  }
 };
+createGame();
 
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
+// Refresh button
+const refreshButton = document.querySelector('.refresh');
+refreshButton.addEventListener('click', async () => {
+  const scores = await getGameScores(gameId);
+  const scoresList = document.getElementById('scores');
+  scoresList.innerHTML = '';
+  scores.forEach((score) => {
+    const li = createGameScores(score.user, score.score);
+    scoresList.appendChild(li);
+  });
+});
 
+// Submit button
+const submitButton = document.querySelector('.submit');
+submitButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
   const name = nameInput.value;
   const score = scoreInput.value;
-
-  // Add new score to the list
-  const newScore = createScores(name, score);
-  scoresList.appendChild(newScore);
-
-  // clear the input fields
+  await saveGameScore(gameId, name, score);
   nameInput.value = '';
   scoreInput.value = '';
 });
